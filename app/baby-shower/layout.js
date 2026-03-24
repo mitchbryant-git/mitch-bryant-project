32acc592f093e00798a49fb5baa9367f3d4e1823
Mitch Bryant <mitch.a.bryant@gmail.com>
2026-03-24 18:23:52 +1000

    babymetadata

---
+++ app/baby-shower/layout.js
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "Alyssa and Mitch's Baby Shower",
+  description: "You're invited to celebrate with us!",
+  openGraph: {
+    title: "Alyssa and Mitch's Baby Shower",
+    description: "You're invited to celebrate with us!",
+    type: "website",
+  },
+};
+
+export default function BabyShowerLayout({ children }) {
+  return children;
+}
