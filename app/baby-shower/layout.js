c2ec34252aa3e6c48dbf65f98b5c774115c3693d
Mitch Bryant <mitch.a.bryant@gmail.com>
2026-04-20 21:19:20 +1000

    baby pic

--- app/baby-shower/layout.js
+++ app/baby-shower/layout.js
@@ -5,6 +5,14 @@ export const metadata = {
     title: "Alyssa and Mitch's Baby Shower",
     description: "You're invited to celebrate with us!",
     type: "website",
+    images: [
+      {
+        url: "https://www.mitchbryant.com/belly2-compressed.jpg",
+        width: 1200,
+        height: 900,
+        alt: "Baby Shower Invitation",
+      },
+    ],
   },
 };
 
